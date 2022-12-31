2021d517ee4f10008d0afd20aace670d4880902e
Robert Pliszka <robplisz@gmail.com>
2022-12-31 15:22:05 -0500

    Moved the functions to the dom-functions.js file

--- secret-message.js
+++ secret-message.js
@@ -5,14 +5,7 @@ const buttonElement = document.getElementById("secret-button");
 // This is the p element
 const pElement = document.getElementById("secret-p");
 
-// this is a function which changes the display content on the page.
-const toggleHiddenElement = (domElement) => {
-  if (domElement.style.display === "none") {
-    domElement.style.display = "block";
-  } else {
-    domElement.style.display = "none";
-  }
-};
+
 
 // Event listener for the button
 buttonElement.addEventListener("click", () => {
