f47a18b055048766b1d8aba5386cabaca3e143ee
Robert Pliszka <robplisz@gmail.com>
2023-01-01 16:39:27 -0500

    Updated the code in the domFunctions.js inside the module directory

---
+++ sites/modules/domFunction.js
@@ -0,0 +1,20 @@
+// function that changes element display from none to block and other way around
+const toggleHiddenElement = (domElement) => {
+  if (domElement.style.display === "none") {
+    domElement.style.display = "block";
+  } else {
+    domElement.style.display = "none";
+  }
+};
+
+// Function to change the background color to random colors.
+const changeToFunkyColor = (domElement) => {
+  const r = Math.random() * 255;
+  const g = Math.random() * 255;
+  const b = Math.random() * 255;
+
+  domElement.style.background = `rgb(${r}, ${g}, ${b})`;
+};
+
+// exporting function to be available to other files in the directory
+export { toggleHiddenElement, changeToFunkyColor };
